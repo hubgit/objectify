501a11e086468ac3c7829a0ca0ce1868a6b55b7f
Alf Eaton <alf@hubmed.org>
2013-07-19 12:11:06 +0100

    escape quotes

--- phparise.js
+++ phparise.js
@@ -2,10 +2,14 @@ var phparise = function(obj) {
 	var output = [];
 
 	for (var key in obj) {
-	   if (obj.hasOwnProperty(key)) {
-	      output.push("'" + key + "' => '" + obj[key] + "'");
-	   }
+		if (obj.hasOwnProperty(key)) {
+			output.push("'" + key + "' => '" + escape_quote(obj[key]) + "'");
+		}
 	}
 
 	return "array(\n\t" + output.join(",\n\t") + "\n)";
 };
+
+var escape_quote = function(text) {
+	return text.replace(/\'/g, "\\'");
+}
